9b8bb9b6311fe299eccef14d4cd6d882d6f45384
noda-dev <kota13011726@icloud.com>
2020-04-11 04:06:04 +0900

    時計作成しました

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type Props = {}
+
+type State = {
+  hour: string;
+  min: string;
+  sec: string;
+}
+
+class App extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    const d:Date = new Date();
+    const hour = d.getHours();
+    const min = d.getMinutes();
+    const sec = d.getSeconds();
+
+    const time = this.adjDigit(hour, min, sec);
+    
+    this.state = {
+      hour: time[0],
+      min: time[1],
+      sec: time[2]
+    }
+  }
+
+  setHour = ():void => {
+    const d:Date = new Date();
+    const hour = d.getHours();
+    const min = d.getMinutes();
+    const sec = d.getSeconds();
+
+    const time = this.adjDigit(hour, min, sec);
+
+    this.setState({
+      hour: time[0],
+      min: time[1],
+      sec: time[2]
+    })
+  }
+
+  adjDigit = (hour:number, min:number, sec:number):string[] => {
+    const time = [hour, min, sec];
+    const adjtime = time.map((val) => {
+      if(String(val).length < 2) {
+        return '0' + String(val);
+      }
+      return String(val);
+    });
+    return adjtime;
+  }
+
+  componentDidMount() {
+    setInterval(this.setHour, 1000);
+  }
+
+  render() {
+    return(
+      <div>{this.state.hour}:{this.state.min}:{this.state.sec}</div>
+    )
+  }
+
 }
 
 export default App;
